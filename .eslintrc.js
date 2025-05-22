81e80881d9cdeb13f0eac5d53245483a6526605c
jjamming <ohj3694@naver.com>
2025-05-22 15:31:26 +0900

    chore: ESLint & Prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended', // prettier랑 통합
+  ],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-
 function App() {
-
   return (
     <>
       <h1>main</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
