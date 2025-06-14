ff0a694f22d1a3bdb5c43d47e6403ff31431cb67
jjamming <ohj3694@naver.com>
2025-06-14 21:27:34 +0900

    feat: title 컴포넌트 생성

---
+++ src/_BacktestingPage/components/Title.tsx
@@ -0,0 +1,11 @@
+const Title = () => {
+  return (
+    <div className="mb-4">
+      <div className="border-white border-b font-suit font-bold text-[2.5rem]">
+        자산배분 백테스트
+      </div>
+    </div>
+  );
+};
+
+export default Title;
