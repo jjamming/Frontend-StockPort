aeefa3569bed23e9a310ea6dba2784895101d194
jjamming <ohj3694@naver.com>
2025-06-14 22:18:09 +0900

    chore: backtestForm 타입 정의

---
+++ src/_BacktestingPage/types/backtestFormType.ts
@@ -0,0 +1,16 @@
+export type BacktestFormValues = {
+  start_date: string; // "2010-01-01"
+  end_date: string; // "2020-12-31"
+  rebalance_frequency: "매년" | "분기별" | "매월";
+  initial_amount: number;
+};
+
+export type Asset = {
+  ticker: string;
+  name: string;
+  weight: number;
+};
+
+export type BacktestRequest = BacktestFormValues & {
+  assets: Asset[];
+};
