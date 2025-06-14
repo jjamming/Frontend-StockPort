0facd369b4510c414dfdaa9b3a7baf760d945de1
jjamming <ohj3694@naver.com>
2025-06-14 21:37:25 +0900

    feat: Notices.tsx 구현

--- src/pages/BacktestingPage.tsx
+++ src/pages/BacktestingPage.tsx
@@ -1,9 +1,11 @@
 import Title from "@/_BacktestingPage/components/Title";
+import Notice from "@/_BacktestingPage/components/Notice";
 
 const BacktestingPage = () => {
   return (
-    <div className="px-18">
+    <div className="gap-2 px-18">
       <Title></Title>
+      <Notice></Notice>
     </div>
   );
 };
