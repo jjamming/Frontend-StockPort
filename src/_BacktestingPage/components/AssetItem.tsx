537629f72e9fb19d9acb4d488f4fd78adb3e0390
jjamming <ohj3694@naver.com>
2025-06-15 16:12:03 +0900

    feat: 자산2부터 삭제버튼 렌더링되도록 적용

--- src/_BacktestingPage/components/AssetItem.tsx
+++ src/_BacktestingPage/components/AssetItem.tsx
@@ -6,8 +6,17 @@ type AssetItemProps = {
 const AssetItem = ({ AssetIndex, onDelete }: AssetItemProps) => {
   return (
     <div className="flex mx-10">
-      <div className="w-55 font-suit text-2xl">자산 {AssetIndex + 1}</div>
-      <button onClick={onDelete}>삭제</button>
+      <div className="flex items-center w-55 font-suit text-2xl">
+        자산 {AssetIndex + 1}
+      </div>
+      {AssetIndex !== 0 ? (
+        <button
+          className="flex justify-center items-center hover:bg-blue-950 px-4 py-1 rounded-3xl font-suit text-l text-white"
+          onClick={onDelete}
+        >
+          X
+        </button>
+      ) : undefined}
     </div>
   );
 };
