c8aa5559697c87f865e02735052edc455f1297e4
jjamming <ohj3694@naver.com>
2025-09-01 14:29:26 +0900

    chore: 게시글 목데이터 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,13 +1,14 @@
 import HeroSection from "@/_MainPage/components/HeroSection";
 import HotPostsSection from "@/_MainPage/components/HotPostSection";
 import MarketIndexSection from "@/_MainPage/components/MarketIndexSection";
+import { MOCK_POSTS } from "@/_CommunityPage/datas/mockPosts";
 
 const MainPage = () => {
   return (
     <div>
       <HeroSection></HeroSection>
       <MarketIndexSection></MarketIndexSection>
-      <HotPostsSection></HotPostsSection>
+      <HotPostsSection posts={MOCK_POSTS}></HotPostsSection>
     </div>
   );
 };
