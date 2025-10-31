57f0ab71ea68deeb3be94527379af2585b7ceb5a
jjamming <ohj3694@naver.com>
2025-10-31 21:49:39 +0900

    fix: 기획에서 제거된 페이지 라우팅 제거

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,19 +1,8 @@
 import { type RouteObject } from "react-router-dom";
 import Layout from "../layouts/Layout";
-import {
-  MainPage,
-  MarketsPage,
-  MarketDetailPage,
-  CommunityPage,
-  PortfolioPage,
-  BacktestingPage,
-  LoginPage,
-  SignUpPage,
-} from "../pages";
+import { MainPage, MarketsPage, MarketDetailPage, PortfolioPage, BacktestingPage } from "../pages";
 
 export const routes: RouteObject[] = [
-  { path: "/login", element: <LoginPage /> },
-  { path: "/signup", element: <SignUpPage /> },
   {
     path: "/",
     element: <Layout />,
@@ -21,7 +10,6 @@ export const routes: RouteObject[] = [
       { path: "", element: <MainPage /> },
       { path: "markets", element: <MarketsPage /> },
       { path: "markets/:code", element: <MarketDetailPage /> },
-      { path: "community", element: <CommunityPage /> },
       { path: "portfolio", element: <PortfolioPage /> },
       { path: "backtest", element: <BacktestingPage /> },
     ],
